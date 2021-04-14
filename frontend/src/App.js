87accab32e0829848b927bf6d6044430b54686a8
emersonsaf <emersonfilho3@gmail.com>
2021-04-14 19:47:29 -0300

    add Routes and Page NotFound

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,23 @@ import AboutUs from "./pages/AboutUs";
 
 import GlobalStyle from './components/GlobalStyle';
 
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import NotFound from "./pages/NotFound";
+
 function App() {
   return (
     <div>
-      <GlobalStyle />
-      <AboutUs />
+      <Router>
+        <GlobalStyle />
+        <Switch>
+          <Route exact path='/'>
+            <AboutUs />
+          </Route>
+          <Route>
+            <NotFound />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ frontend/src/pages/NotFound.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { About, Description , Image} from '../styles'
+
+const NotFound = () => {
+    return (
+        <About>
+            <Description>
+                <h2>404</h2>
+                <h2>PAGE NOT FOUNT</h2>
+            </Description>
+        </About>
+    )
+}
+
+export default NotFound
