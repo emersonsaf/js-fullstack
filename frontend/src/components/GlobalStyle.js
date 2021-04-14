38deaf4545f4bbbb054f8212e173d8c2ee6f0b15
emersonsaf <emersonfilho3@gmail.com>
2021-04-14 19:18:25 -0300

    add styles and service section

--- frontend/src/components/GlobalStyle.js
+++ frontend/src/components/GlobalStyle.js
@@ -5,59 +5,59 @@ const GlobalStyle = createGlobalStyle`
         margin: 0;
         padding: 0;
         box-sizing: border-box;
-
     }
-
+    html{
+        @media (max-width: 1700px){
+            font-size: 75%;
+        }
+    }
     body{
         background: #1b1b1b;
-        overflorw-x: hidden;
+        overflow-x: hidden;
         text-decoration: none;
     }
-
     button{
         font-weight: bold;
-        font-size: 1.1rem;
+        font-size: 1.1.rem;
         cursor: pointer;
         padding: 1rem 2rem;
+        border: 3px solid #416CD5;
         background: transparent;
         color: yellow;
-        border: 3px solid #416cd5;
         transition: all 0.5s ease;
         &:hover{
-            background-color: #23b997;
+            background-color: #23d997;
             color: white;
         }
     }
-
     h2{
         font-weight: lighter;
         font-size: 4rem;
     }
-
     h3{
         color: white;
     }
-
     h4{
-        font-weight: bold;
+        font-weight:bold;
         font-size: 1.2rem;
         color: lightgrey;
     }
-
     a{
-        font-size: 1.1rem;
+        font-size: 1.1.rem;
     }
-
     span{
         font-weight: lighter;
         color: #416CD5;
     }
-
     p{
         padding: 3rem 0rem;
         color: #ccc;
         font-size: 1.4rem;
         line-height: 150%;
+    }
+    label{
+        color: black;
+        font-weight: bold;
     }
 `;
 
--- frontend/src/components/ServiceSection.js
+++ frontend/src/components/ServiceSection.js
@@ -4,45 +4,49 @@ import styled from 'styled-components'
 
 import { About, Description, Image } from '../styles'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGraduationCap, faImage, faLaptopCode } from '@fortawesome/free-solid-svg-icons'
+import { faGithubSquare } from '@fortawesome/free-brands-svg-icons'
+
 const ServiceSection = () => {
     return (
         <Services>
             <Image>
                 <img src="https://sites.code.education/wp-content/uploads/2019/02/Mservices.png" />
             </Image>
-            <Description>
+            <ServiceDescription>
                 <h2>Things I do</h2>
                 <Cards>
                     <Card>
                         <div className='icon'>
-                            ICONE
+                        < FontAwesomeIcon icon={faLaptopCode} size='4x'/>
                         <h3>Portfolio</h3>
                         </div>
-                    <p>Check my work on my portfolio</p>
+                        <p>Check my work on my portfolio</p>
                     </Card>
                     <Card>
                         <div className='icon'>
-                            ICONE
+                        < FontAwesomeIcon icon={faGraduationCap} size='4x'/>
                         <h3>Courses</h3>
                         </div>
-                    <p>Check my work on my portfolio</p>
+                        <p>Check my work on my portfolio</p>
                     </Card>
                     <Card>
                         <div className='icon'>
-                            ICONE
+                        < FontAwesomeIcon icon={faImage} size='4x'/>
                         <h3>Hobbies</h3>
                         </div>
-                    <p>Check my work on my portfolio</p>
+                        <p>Check my work on my portfolio</p>
                     </Card>
                     <Card>
                         <div className='icon'>
-                            ICONE
-                        <h3>Github</h3>
+                            < FontAwesomeIcon icon={faGithubSquare} size='4x'/>
+                            <h3>Github</h3>
                         </div>
-                    <p>Check my work on my portfolio</p>
+                        <p>Check my work on my portfolio</p>
                     </Card>
                 </Cards>
-            </Description>
+            </ServiceDescription>
         </Services>
     )
 }
@@ -53,20 +57,22 @@ const Services = styled(About)`
     }
     p{
         width: 70%;
-        padding: 2rem 0rem 4rem 0rem;
+        padding: 2rem 0rem 4rem;
     }
 `;
 
 const Cards = styled.div`
-    background: red;
     display: flex;
-    flew-wrap: wrap;
+    flex-wrap: wrap;
     padding: 1rem;
 
 `;
 
+const ServiceDescription = styled(Description)`
+    flex: 2;
+`;
+
 const Card = styled.div`
-    background: green;
     padding: 0.5rem;
     flex-basis: 20rem;
     .icon{
