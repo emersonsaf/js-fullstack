b89bcf248852aa4d4b4ab1cbe5b32f19c9669cb9
emersonsaf <emersonfilho3@gmail.com>
2021-05-25 21:24:18 -0300

    change some css files, and customize service section

--- frontend/src/components/ServiceSection.js
+++ frontend/src/components/ServiceSection.js
@@ -22,28 +22,29 @@ const ServiceSection = () => {
                         < FontAwesomeIcon icon={faLaptopCode} size='4x'/>
                         <h3>Portfolio</h3>
                         </div>
-                        <p>Check my work on my portfolio</p>
+                        <p>Check my work on my portfolio page.</p>
                     </Card>
                     <Card>
                         <div className='icon'>
                         < FontAwesomeIcon icon={faGraduationCap} size='4x'/>
                         <h3>Courses</h3>
                         </div>
-                        <p>Check my work on my portfolio</p>
+                        <p>Ruby, PHP, Python, ReactJS and NodeJS.</p>
                     </Card>
                     <Card>
                         <div className='icon'>
                         < FontAwesomeIcon icon={faImage} size='4x'/>
                         <h3>Hobbies</h3>
                         </div>
-                        <p>Check my work on my portfolio</p>
+                        <p>I like to play guitar, drawing and play some eletronic games.</p>
+
                     </Card>
                     <Card>
                         <div className='icon'>
                             < FontAwesomeIcon icon={faGithubSquare} size='4x'/>
                             <h3>Github</h3>
                         </div>
-                        <p>Check my work on my portfolio</p>
+                        <p>See more code in my profile emersonsaf on Github.</p> 
                     </Card>
                 </Cards>
             </ServiceDescription>
